b11167eb9aacdcc4d26ffa03061b2215041c5a17
Hannah Thoreson Godofsky <60587547+brickstackr@users.noreply.github.com>
2021-10-04 23:10:06 -0400

    card updates
    card

--- src/components/CardCarousel/CardCarousel.tsx
+++ src/components/CardCarousel/CardCarousel.tsx
@@ -4,30 +4,33 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Box, Pagination } from '@mui/material';
+import { Grid } from '@mui/material';
 
 export default function CardCarousel() {
   return (
+    <>
     <Box>
-    <Card sx={{ maxWidth: 800 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="500"
-          image="demo-images/demo1.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Pagination count={10} variant="outlined" color="primary" />
-    </Box>
+          <Card sx={{ maxWidth: 800 }}>
+              <CardActionArea>
+                  <CardMedia
+                      component="img"
+                      height="500"
+                      image="demo-images/demo1.jpg"
+                      alt="green iguana" />
+                  <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                          Siemens PLC
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                          Siemens PLC for industrial automation
+                      </Typography>
+                  </CardContent>
+              </CardActionArea>
+          </Card>
+      </Box>
+      <Grid p={1}>
+              <Pagination count={3} variant="outlined" color="primary" />
+        </Grid>
+        </>
   );
 }
